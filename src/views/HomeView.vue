8bd2534d29db69c3aff332dfd741e38b164a80ff
Rodjean Verzosa <161000666+rodjean015@users.noreply.github.com>
2024-04-13 16:45:15 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="container">
-    <h1 class="position-absolute top-50 start-50 translate-middle display-2">Welcome to Cipher Academy</h1>
-    
+  <div class="container mt-5">
+    <h1 class="display-2">Welcome to Cipher Academy</h1>
+    <h2 class="display-4">Go to Student TAB <RouterLink to="/students/view"><span class="material-symbols-outlined">arrow_forward</span></RouterLink></h2>
+    <p class="display-5 mt-5">Open this file on vs.code <a href="https://github.com/rodjean015/cipher/tree/main/cipher_backend" class="btn btn-primary">code</a></p>
+    <p class="lead"><span class="material-symbols-outlined text-success">check_circle</span> Edit this directory DB_DATABASE=cipher_backend\database\cipher_academy.sqlite</p>
+    <p class="lead"><span class="material-symbols-outlined text-success">check_circle</span> Or Create Database in Localhost with name "cipher_academy"</p>
+    <p class="lead"><span class="material-symbols-outlined text-success">check_circle</span> And enter command php artisan serve on terminal.</p>
   </div>
 </template>
